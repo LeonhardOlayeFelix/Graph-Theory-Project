052606aea15c428b978fb27111a3c4fd1c61394a
Leonhard <97770986+LeonhardOlayeFelix@users.noreply.github.com>
2022-04-09 15:13:52 +0100

    -Added linked list node class for DFS

---
+++ Interface 2/LinkedListsClasses/LinkedListNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_2.LinkedListsClasses
+{
+    class LinkedListNode
+    {
+        public int data;
+        public LinkedListNode next;
+
+        public LinkedListNode(int v)
+        {
+            data = v;
+            next = null;
+        }
+    }
+}
