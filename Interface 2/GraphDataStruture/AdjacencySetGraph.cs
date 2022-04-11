0cf38547869bc32703873bfa5fe7bc29f459f964
Leonhard <97770986+LeonhardOlayeFelix@users.noreply.github.com>
2022-04-11 17:22:39 +0100

    Commenting

--- Interface 2/GraphDataStruture/AdjacencySetGraph.cs
+++ Interface 2/GraphDataStruture/AdjacencySetGraph.cs
@@ -759,43 +759,47 @@ namespace Interface_2
             }
             return index;//return the index that the lowest combination is at
         }
-        public Tuple<List<Tuple<int, int>>, string> BreadthFirst(int startNode)/*Step 1: Choose any one node randomly, to start traversing.
-                                                                Step 2: Visit its adjacent unvisited node.
-                                                                Step 3: Mark it as visited in the boolean array and display it.
-                                                                Step 4: Insert the visited node into the queue.
-                                                                Step 5: If there is no adjacent node, remove the first node from the queue.
-                                                                Step 6: Repeat the above steps until the queue is empty.*/
+        public Tuple<List<Tuple<int, int>>, string> BreadthFirst(int startNode)//returns a Tuple :
+                                                                               //Item1: List of tuples (item1 vertex, and item 2 its parent)
+                                                                               //Item2: Traversal Order String
+                                                                            /*Step 1: Choose any one node randomly, to start traversing.
+                                                                            Step 2: Visit its adjacent unvisited node.
+                                                                            Step 3: Mark it as visited in the boolean array and display it.
+                                                                            Step 4: Insert the visited node into the queue.
+                                                                            Step 5: If there is no adjacent node, remove the first node from the queue.
+                                                                            Step 6: Repeat the above steps until the queue is empty.*/
         {
-            UDLinkedList queue = new UDLinkedList();
-            List<Tuple<int, int>> visited = new List<Tuple<int, int>>();
-            List<int> outputList = new List<int>();
-            visited.Add(Tuple.Create(startNode, -1));
-            outputList.Add(startNode);
-            List<int> adjNodes = GetAdjVertices(startNode);
+            UDLinkedList queue = new UDLinkedList(); //create an instance of the linked list for the queue
+            List<Tuple<int, int>> visited = new List<Tuple<int, int>>(); //intialise return value item
+            List<int> outputList = new List<int>();//initialise traversal order return value item
+            visited.Add(Tuple.Create(startNode, -1));//start node doesnt have a parent so we can set it to -1 and mark it as visited
+            outputList.Add(startNode); //add the node to the output list
+            List<int> adjNodes = GetAdjVertices(startNode); //get the nodes that are adjacent to that start node
             for (int i = 0; i < adjNodes.Count(); ++i)
             {
-                queue.Append(Tuple.Create(adjNodes[i], startNode));
+                queue.Append(Tuple.Create(adjNodes[i], startNode)); //add all the adjacent nodes to the queue
             }
-            while (queue.Count != 0)
+            while (queue.Count != 0)//do this until the stack is empty
             {
-                Tuple<int, int> topOfQueue = queue.PopFront();
-                visited.Add(topOfQueue);
-                int parentNode = topOfQueue.Item1;
+                Tuple<int, int> topOfQueue = queue.PopFront(); //take and save the vertex at the front of the list
+                visited.Add(topOfQueue); //mark this vertex as visited
+                int parentNode = topOfQueue.Item1; //the parent of the next-generated adj vertices will be item1 of this vertex
                 outputList.Add(parentNode);
-                adjNodes = GetAdjVertices(parentNode);
-                for (int i = 0; i < adjNodes.Count(); ++i)
+                adjNodes = GetAdjVertices(parentNode);//get the adjacent vertices
+                for (int i = 0; i < adjNodes.Count(); ++i)//loop through the adjacent vertices
                 {
-                    if (!NodeVisited(visited, adjNodes[i]) && !queue.Contains(adjNodes[i]))
-                        queue.Append(Tuple.Create(adjNodes[i], parentNode));
+                    if (!NodeVisited(visited, adjNodes[i]) && !queue.Contains(adjNodes[i]))//if not visited and not already in queue
+                        queue.Append(Tuple.Create(adjNodes[i], parentNode));//append them onto the stack
                 }
             }
-            visited.RemoveAt(0);
-            return Tuple.Create(visited, String.Join(", ", outputList));
-;
+            visited.RemoveAt(0); //remove (startvertex, -1) from the list since its not an edge
+            return Tuple.Create(visited, String.Join(", ", outputList)); //return the ordered edges and 
         }
         public Tuple<List<Tuple<int, int>>, bool, string> DepthFirst(int startNode) //returns a Tuple :
                                                                             //Item1: List of tuples (item1 vertex, and item 2 its parent)
                                                                             //Item2: bool which represents whether a cycle was found
+                                                                            //Item3: Traversal Order String
+
                                                                             /*Step 1: Start by putting any one of the graph's vertices 
                                                                                       on top of a stack.
                                                                               Step 2: Take the top item of the stack and add it to the visited list.
@@ -804,7 +808,7 @@ namespace Interface_2
                                                                               Step 4: Keep repeating steps 2 and 3 until the stack is empty.*/
         {
             List<Tuple<int, int>> visited = new List<Tuple<int, int>>(); //initialise return value
-            List<int> outputList = new List<int>();
+            List<int> outputList = new List<int>();//initialise traversal order return value item
             visited.Add(Tuple.Create(startNode, -1)); //start node doesnt have a parent so we can set it to -1 and mark it as visited
             outputList.Add(startNode);
             List<int> adjNodes = GetAdjVertices(startNode); //get the nodes that are adjacent to that start node
@@ -832,7 +836,7 @@ namespace Interface_2
                 }
             }
             visited.RemoveAt(0); //remove (startvertex, -1) from the list since its not an edge
-            return Tuple.Create(visited, cycleExists, String.Join(",", outputList)); //return the ordered edges, and cycle
+            return Tuple.Create(visited, cycleExists, String.Join(",", outputList)); //return the ordered edges, the cycle and the traversal order
         }
         private bool NodeVisited(List<Tuple<int, int>> visited, int node) //retunrs whether a node has been visited
         {
